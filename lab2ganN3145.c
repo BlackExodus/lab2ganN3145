1d37587a3eab5613e5a1d76030edc36bd906196b
ACEY <grg1nev@gmail.com>
2023-12-14 19:49:03 +0300

    Made all?

--- lab2ganN3145.c
+++ lab2ganN3145.c
@@ -2,36 +2,105 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <time.h>
+#include <limits.h>
+
+bool isnum(char* n) {
+    for(int i = 0; i < (int)strlen(n); ++i)
+        if(!isdigit(n[i]) && n[i] != ' ')
+            return false;
+    return true;
+}
 
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
     // Проверка запуска с переменной среды, включающей отладочный вывод.
     // Пример запуска с установкой переменной LAB2DEBUG в 1:
     // $ LAB2DEBUG=1 ./lab2abcNXXXXX 5 5
+
     char *DEBUG = getenv("LAB2DEBUG");
     if (DEBUG) {
         fprintf(stderr, "Включен вывод отладочных сообщений\n");
     }
 
-    if ((argc != 4 || strcmp(argv[1], "-m")) && (argc != 3)) {
-        fprintf(stderr, "Usage: %s [-m] число_строк число_столбцов\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    
     //
     // Тут может быть ваш код. В этом файле Вы можете поменять все, что угодно.
     // Главное - чтобы потом все правильно работало ;-)
     //
 
-    int m = atoi(argv[2]), n = atoi(argv[3]);
-    //printf("Your matrix dimensions: %d and %d\n", m, n);
-    printf("Введите строки матрицы:\n");
-    fflush(stdout);
+    //char *newInput;
+    int m = 0, n = 0;
+    char* standartOpType = "-m";
+
+    bool set_matrix = false;
+
+    if(argc < 2) {
+        printf("Ошибка: отсутствуют обязательные аргументы");
+        return EXIT_FAILURE;
+    }
+    char *opType = argv[1];
+    if(isnum(opType) && argc == 3) {
+        m = atoi(opType);
+        opType= "";
+        if(isnum(argv[2]))
+            n = atoi(argv[2]);
+        else {
+            printf("Ошибка: '%s' не является числом\n", argv[2]);
+            return EXIT_FAILURE;
+        }
+        printf("m = %d, n = %d\n", m, n);
+    }
+    else if(strcmp(opType, standartOpType) == 0) {
+        set_matrix = true;
+        if(isnum(argv[2]))
+            m = atoi(argv[2]);
+        else {
+            printf("Ошибка: '%s' не является числом\n", argv[2]);
+            return EXIT_FAILURE;
+        }
+
+        if(isnum(argv[3]))
+            n = atoi(argv[3]);
+        else {
+            printf("Ошибка: '%s' не является числом\n", argv[3]);
+            return EXIT_FAILURE;
+        }
+    }
+    else if(strcmp(opType, standartOpType) != 0) {
+        printf("Ошибка: опция '%s' не поддерживается.", opType);
+        return EXIT_FAILURE;
+    }
+
+    if(!(m > 0 && n > 0)){
+        printf("Ошибка: '%d' не является натуральным числом", m <= 0 ? m : n);
+        return EXIT_FAILURE;
+    }
 
     short matrix[m][n];
-    for(int i = 0; i < m; ++i)
-        for(int j = 0; j < n; ++j)
-            scanf("%hd", &matrix[i][j]);
+    if(set_matrix){
+        printf("Введите строки матрицы:\n");
+        fflush(stdout);
 
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                char q[5];
+                scanf("%s", q);
+                if(isnum(q))
+                    matrix[i][j] = atoi(q);
+                else{
+                    printf("Ошибка: '%s' не является числом\n", q);
+                    return EXIT_FAILURE;
+                }
+            }
+        }
+    }
+    else{
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                matrix[i][j] = (short)(rand() % RAND_MAX);
+    }
 
     printf("Исходная матрица:\n");
     for(int i = 0; i < m; ++i) {
@@ -44,16 +113,18 @@ int main(int argc, char *argv[]) {
     printf("Введите матрицу P 2*2:\n");
     fflush(stdout);
     short p1, p2, p3, p4;
-    scanf("%hd %hd %hd %hd", &p1, &p2, &p3, &p4);
+    int q = scanf("%hd %hd %hd %hd", &p1, &p2, &p3, &p4);
+    if(q != 4){
+        printf("Вы неправильно ввели матрицу P");
+        return EXIT_FAILURE;
+    }
 
     printf("Матрица P:\n%hd %hd\n%hd %hd\n", p1, p2, p3, p4);
     fflush(stdout);
 
-    int x = -1, y = -1;
     for(int i = 0; i < m - 1; ++i){
         for(int j = 0; j < m - 1; ++j){
             if(matrix[i][j] == p1 && matrix[i][j + 1] == p2 && matrix[i + 1][j] == p3 && matrix[i + 1][j + 1] == p4){
-                x = j, y = i;
                 matrix[i][j] = pow(matrix[i][j], 2);
                 matrix[i][j+1] = pow(matrix[i][j+1], 2);
                 matrix[i+1][j] = pow(matrix[i+1][j], 2);
@@ -67,14 +138,13 @@ int main(int argc, char *argv[]) {
     printf("Соответствий не найдено ¯\\_(ツ)_/¯");
     return EXIT_FAILURE;
 
-    success:
-    printf("Соотвествие найдено!\nX = %x, Y = %d\n", x, y);
-    printf("Обновленная матрица:\n");
+
+success:
+    printf("Результат:\n");
     for(int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j)
             printf("%hd ", matrix[i][j]);
         printf("\n");
     }
-    
     return EXIT_SUCCESS;
 }
